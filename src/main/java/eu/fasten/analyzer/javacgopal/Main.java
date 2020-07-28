a7434625e5a4a2407e8d8df102e8bf6b31aaefa8
Ilya Grishkov <ilyagrishkov@icloud.com>
2020-07-28 10:55:02 +0300

    Opal plugin with all dependencies

---
+++ src/main/java/eu/fasten/analyzer/javacgopal/Main.java
@@ -0,0 +1,288 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.fasten.analyzer.javacgopal;
+
+import eu.fasten.analyzer.javacgopal.core.RevisionCallGraph;
+import eu.fasten.analyzer.javacgopal.data.MavenCoordinate;
+import eu.fasten.analyzer.javacgopal.data.PartialCallGraph;
+import eu.fasten.analyzer.javacgopal.merge.CallGraphMerger;
+import eu.fasten.analyzer.javacgopal.merge.CallGraphUtils;
+import java.io.File;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+
+/**
+ * Makes javacg-opal module runnable from command line.
+ */
+@CommandLine.Command(name = "JavaCGOpal")
+public class Main implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    @CommandLine.ArgGroup(exclusive = true, multiplicity = "1")
+    Commands commands;
+
+    @CommandLine.Option(names = {"-o", "--output"},
+        paramLabel = "OUT",
+        description = "Output directory path")
+    String output;
+
+    static class Commands {
+
+        @CommandLine.ArgGroup(exclusive = false)
+        Computations computations;
+
+    }
+
+    static class Computations {
+
+        @CommandLine.Option(names = {"-a", "--artifact"},
+            paramLabel = "ARTIFACT",
+            description = "Artifact, maven coordinate or file path")
+        String artifact;
+
+        @CommandLine.Option(names = {"-r"},
+            paramLabel = "REPOS",
+            description = "Maven repositories",
+            split = ",")
+        List<String> repos;
+
+        @CommandLine.Option(names = {"-t", "--timestamp"},
+            paramLabel = "TS",
+            description = "Release TS",
+            defaultValue = "0")
+        String timestamp;
+
+        @CommandLine.Option(names = {"-m", "--mode"},
+            paramLabel = "MODE",
+            description = "Input of algorithms are {FILE or COORD}",
+            defaultValue = "FILE")
+        String mode;
+
+        @CommandLine.ArgGroup(exclusive = true)
+        Tools tools;
+    }
+
+    static class Tools {
+
+        @CommandLine.ArgGroup(exclusive = false)
+        Opal opal;
+
+        @CommandLine.ArgGroup(exclusive = false)
+        Merge merge;
+    }
+
+    static class Opal {
+
+        @CommandLine.Option(names = {"-g", "--generate"},
+            paramLabel = "GEN",
+            description = "Generate call graph for artifact")
+        boolean doGenerate;
+
+
+    }
+
+    static class Merge {
+
+        @CommandLine.Option(names = {"-s", "--stitch"},
+            paramLabel = "STITCH",
+            description = "Stitch artifact CG to dependencies")
+        boolean doMerge;
+
+        @CommandLine.Option(names = {"-d", "--dependencies"},
+            paramLabel = "DEPS",
+            description = "Dependencies, coordinates or files",
+            split = ",")
+        List<String> dependencies;
+
+        @CommandLine.Option(names = {"-l", "--algorithm"},
+            paramLabel = "ALG",
+            description = "Merge Tools {RA or CHA}",
+            defaultValue = "CHA")
+        String algorithm;
+
+    }
+
+    /**
+     * Generates RevisionCallGraphs using Opal for the specified artifact in the command line
+     * parameters.
+     */
+    public static void main(String[] args) {
+        new CommandLine(new Main()).execute(args);
+    }
+
+    public void run() {
+        if (this.commands.computations != null && this.commands.computations.tools != null) {
+            if (this.commands.computations.tools.opal != null
+                && this.commands.computations.tools.opal.doGenerate) {
+
+                if (commands.computations.mode.equals("COORD")) {
+                    final var artifact = getArtifactCoordinate();
+                    logger.info("Generating call graph for the Maven coordinate: {}", artifact);
+                    try {
+                        if (this.output != null) {
+                            generate(artifact, true);
+                        } else {
+                            System.out.println(generate(artifact, false).toJSON().toString(4));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                } else if (commands.computations.mode.equals("FILE")) {
+                    try {
+                        if (this.output != null) {
+                            generate(getArtifactFile(), true);
+                        } else {
+                            System.out.println(generate(getArtifactFile(), false).toJSON().toString(4));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+            if (this.commands.computations.tools.merge != null
+                && this.commands.computations.tools.merge.doMerge) {
+
+                if (commands.computations.mode.equals("COORD")) {
+                    try {
+                        merge(getArtifactCoordinate(), getDependenciesCoordinates());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                } else if (commands.computations.mode.equals("FILE")) {
+                    try {
+                        merge(getArtifactFile(), getDependenciesFiles()).toJSON();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    public <T> RevisionCallGraph merge(final T artifact,
+                                       final List<T> dependencies) throws IOException {
+
+        final RevisionCallGraph result;
+        final var deps = new ArrayList<RevisionCallGraph>();
+        for (final var dep : dependencies) {
+            deps.add(generate(dep, false));
+        }
+        final var art = generate(artifact, false);
+        result = CallGraphMerger.mergeCallGraph(art, deps,
+            this.commands.computations.tools.merge.algorithm);
+
+        if (this.output != null) {
+            if (result != null) {
+                CallGraphUtils.writeToFile(this.output, result.toJSON().toString(4), "");
+            }
+        } else {
+            System.out.println(result.toJSON().toString(4));
+        }
+
+        return result;
+    }
+
+    public <T> RevisionCallGraph generate(final T artifact, final boolean writeToFile)
+        throws IOException {
+        final RevisionCallGraph revisionCallGraph;
+
+        final long startTime = System.currentTimeMillis();
+
+        if (artifact instanceof File) {
+            logger.info("Generating graph for {}", ((File) artifact).getAbsolutePath());
+            final var cg = new PartialCallGraph((File) artifact);
+            revisionCallGraph =
+                RevisionCallGraph.extendedBuilder().graph(cg.getGraph())
+                    .product(((File) artifact).getName().replace(".class", "").replace("$", ""))
+                    .version("").timestamp(0).cgGenerator("").depset(new ArrayList<>()).forge("")
+                    .classHierarchy(cg.getClassHierarchy()).build();
+        } else {
+            revisionCallGraph =
+                PartialCallGraph.createExtendedRevisionCallGraph((MavenCoordinate) artifact,
+                    Long.parseLong(this.commands.computations.timestamp));
+
+        }
+
+        logger.info("Generated the call graph in {} seconds.",
+            new DecimalFormat("#0.000").format((System.currentTimeMillis() - startTime) / 1000d));
+
+        if (writeToFile) {
+            CallGraphUtils
+                .writeToFile(this.output, revisionCallGraph.toJSON().toString(4), "");
+        }
+        return revisionCallGraph;
+    }
+
+    private List<File> getDependenciesFiles() {
+        final var result = new ArrayList<File>();
+        if (this.commands.computations.tools.merge.dependencies != null) {
+            for (String currentCoordinate : this.commands.computations.tools.merge.dependencies) {
+                result.add(new File(currentCoordinate));
+            }
+        }
+        return result;
+    }
+
+    private List<MavenCoordinate> getDependenciesCoordinates() {
+        final var result = new ArrayList<MavenCoordinate>();
+        if (this.commands.computations.tools.merge.dependencies != null) {
+            for (String currentCoordinate : this.commands.computations.tools.merge.dependencies) {
+                var coordinate = MavenCoordinate.fromString(currentCoordinate);
+                if (this.commands.computations.repos != null
+                    && this.commands.computations.repos.size() > 0) {
+                    coordinate.setMavenRepos(this.commands.computations.repos);
+                }
+                result.add(coordinate);
+            }
+        }
+        return result;
+    }
+
+    private File getArtifactFile() {
+        File result = null;
+        if (this.commands.computations.artifact != null) {
+            result = new File(this.commands.computations.artifact);
+        }
+        return result;
+    }
+
+    private MavenCoordinate getArtifactCoordinate() {
+        MavenCoordinate result = null;
+        if (this.commands.computations.artifact != null) {
+            result = MavenCoordinate.fromString(this.commands.computations.artifact);
+            if (this.commands.computations.repos != null
+                && this.commands.computations.repos.size() > 0) {
+                result.setMavenRepos(this.commands.computations.repos);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/eu/fasten/analyzer/javacgopal/OPALPluginTest.java
@@ -0,0 +1,141 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.fasten.analyzer.javacgopal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import eu.fasten.analyzer.javacgopal.core.RevisionCallGraph;
+import eu.fasten.analyzer.javacgopal.data.MavenCoordinate;
+import eu.fasten.analyzer.javacgopal.data.PartialCallGraph;
+import java.io.FileNotFoundException;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OPALPluginTest {
+
+    static OPALPlugin.OPAL opalPlugin;
+    final String topic = "fasten.maven.pkg";
+
+    @BeforeClass
+    public static void instantiatePlugin() {
+        opalPlugin = new OPALPlugin.OPAL();
+    }
+
+    @Test
+    public void testConsumerTopic() {
+        assertTrue(opalPlugin.consumeTopic().isPresent());
+        assertEquals("fasten.maven.pkg", opalPlugin.consumeTopic().get().get(0));
+    }
+
+    @Test
+    public void testSetTopic() {
+        String topicName = "fasten.mvn.pkg";
+        opalPlugin.setTopic(topicName);
+        assertTrue(opalPlugin.consumeTopic().isPresent());
+        assertEquals(topicName, opalPlugin.consumeTopic().get().get(0));
+    }
+
+    @Test
+    public void testConsume() throws JSONException, FileNotFoundException {
+
+        JSONObject coordinateJSON = new JSONObject("{\n"
+                + "    \"groupId\": \"org.slf4j\",\n"
+                + "    \"artifactId\": \"slf4j-api\",\n"
+                + "    \"version\": \"1.7.29\",\n"
+                + "    \"date\":\"1574072773\"\n"
+                + "}");
+
+        opalPlugin.consume(coordinateJSON.toString());
+
+        assertTrue(opalPlugin.produce().isPresent());
+    }
+
+    @Test
+    public void testShouldNotFaceClassReadingError() throws JSONException, FileNotFoundException {
+
+        JSONObject coordinateJSON1 = new JSONObject("{\n"
+                + "    \"groupId\": \"com.zarbosoft\",\n"
+                + "    \"artifactId\": \"coroutines-core\",\n"
+                + "    \"version\": \"0.0.3\",\n"
+                + "    \"date\":\"1574072773\"\n"
+                + "}");
+
+        opalPlugin.consume(coordinateJSON1.toString());
+
+        assertTrue(opalPlugin.produce().isPresent());
+    }
+
+    @Test
+    public void testEmptyCallGraph() throws JSONException {
+        JSONObject emptyCGCoordinate = new JSONObject("{\n"
+                + "    \"groupId\": \"activemq\",\n"
+                + "    \"artifactId\": \"activemq\",\n"
+                + "    \"version\": \"release-1.5\",\n"
+                + "    \"date\":\"1574072773\"\n"
+                + "}");
+
+        opalPlugin.consume(emptyCGCoordinate.toString());
+        assertTrue(opalPlugin.produce().isPresent());
+        var graph = opalPlugin.produce().get();
+        var cg = new RevisionCallGraph(new JSONObject(graph));
+        //Based on plugin's logs this artifact's call graph should be empty.
+        assertTrue(cg.isCallGraphEmpty());
+    }
+
+    @Test
+    public void testFileNotFoundException() throws JSONException {
+        JSONObject noJARFile = new JSONObject("{\n"
+                + "    \"groupId\": \"com.visionarts\",\n"
+                + "    \"artifactId\": \"power-jambda-pom\",\n"
+                + "    \"version\": \"0.9.10\",\n"
+                + "    \"date\":\"1521511260\"\n"
+                + "}");
+
+        opalPlugin.consume(noJARFile.toString());
+        var error = opalPlugin.getPluginError();
+
+        assertEquals(FileNotFoundException.class.getSimpleName(), error.getClass().getSimpleName());
+    }
+
+    @Test
+    public void testNullPointerException() throws JSONException {
+        JSONObject mvnCoordinate = new JSONObject("{\n"
+                + "    \"groupId\": \"ch.epfl.scala\",\n"
+                + "    \"artifactId\": \"collection-strawman_0.6\",\n"
+                + "    \"version\": \"0.8.0\",\n"
+                + "    \"date\":\"1521511260\"\n"
+                + "}");
+
+//        var cg = opalPlugin.consume(new ConsumerRecord<>(topic, 1, 0, "bar", mvnCoordinate
+//        .toString()), false);
+//        cg.toJSON();
+
+        // TODO: An assert is pointless here. Because we need to find the root cause of the NullPointerException in PartialCallGraph class.
+        // This test shows that FASTEN URIs of a type's methods can be null! Check out the method toListOfString in ExtendedRevisionCallGraph class.
+        // The described problem is patched at the very high level with a if block!
+    }
+
+    @Test
+    public void testName() {
+        assertEquals("eu.fasten.analyzer.javacgopal.OPALPlugin.OPAL", opalPlugin.name());
+    }
+}
